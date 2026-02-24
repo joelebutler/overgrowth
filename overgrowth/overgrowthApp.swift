a13e7cac1058344aab51abbe40f8d5ab79b4a8ce
Joel Butler <90467562+joelebutler@users.noreply.github.com>
2026-02-23 20:22:44 -0500

    Initial Swift App creation

---
+++ overgrowth/overgrowthApp.swift
@@ -0,0 +1,17 @@
+//
+//  overgrowthApp.swift
+//  overgrowth
+//
+//  Created by Joel Butler on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct overgrowthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ overgrowthTests/overgrowthTests.swift
@@ -0,0 +1,17 @@
+//
+//  overgrowthTests.swift
+//  overgrowthTests
+//
+//  Created by Joel Butler on 2/23/26.
+//
+
+import Testing
+@testable import overgrowth
+
+struct overgrowthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
